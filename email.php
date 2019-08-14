6cc7fdc0a247428dd6492a9b916c2cdbbed12a89
Gagandeep Singh <gagandeep16037@iiitd.ac.in>
2019-08-14 15:34:34 +0530

    Email me when someone visits portfolio

--- email.php
+++ email.php
@@ -3,11 +3,17 @@ $name = "User";
 $email = "gagandeep16037@iiitd.ac.in";
 $message = "Your portfolio was visited by someone";
 
-$to = "gagandeep16037@iiitd.ac.in";
-$subject = "User incoming";
-$body = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
-$headers = "From: " . $email;
+// $to = "gagandeep16037@iiitd.ac.in";
+// $subject = "User incoming";
+// $body = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
+// $headers = "From: " . $email;
 
-//send email
-mail($to, $subject, $body, $headers);
+// //send email
+// mail($to, $subject, $body, $headers);
+
+
+    echo $name;
+    echo $email;
+    echo $message
+
 ?>
